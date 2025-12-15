e48dbfe8533e27be2bb85ae50543683aeac0751f
MrunmaiK5 <mrunmai0205@gmail.com>
2025-12-15 20:35:48 +0530

    Added Assignment__40

---
+++ Asignments/Assignment__40/Program40_1.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function name:  Display
+//  Description:    Displays a star pattern till given number by using recursive function.
+//  Input:          Void
+//  Output:         Void
+//  Author:         Mrunmai Jitendra Khadpe
+//  Date:           15/12/2025
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void Display(int iNo)
+{
+    static int iCnt=1;
+
+    if(iCnt<=iNo)
+    {
+        cout << "*\t";
+        iCnt++;
+        Display(iNo);
+    }
+}
+
+int main()
+{
+    int iCnt=0;
+
+    cout<<"Enter a number: \n";
+    cin >> iCnt;
+    Display(iCnt);
+    return 0;
+}
