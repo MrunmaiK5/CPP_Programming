964b4bcec3ece5fc29dfe5fd6f03654e2fcfc467
MrunmaiK5 <mrunmai0205@gmail.com>
2026-02-13 14:26:44 +0530

    Added Assignment__69

---
+++ Assignments/Assignment__69/Program69_2.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function name:  Display
+//  Description:    Displays a number pattern till given number by using recursive function.
+//  Input:          Void
+//  Output:         Void
+//  Author:         Mrunmai Jitendra Khadpe
+//  Date:           13/02/2026
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+    Input : 5
+    Output : 1 2 3 4 5
+*/
+
+void Display(int iNo)
+{
+    static int i = 1;
+
+    if (i <= iNo)
+    {
+        cout << i<< "\t";
+        i++;
+        Display(iNo);
+    }
+    else
+    {
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    cout << "Enter a number : ";
+    cin >> iValue;
+
+    Display(iValue);
+
+    return 0;
+}
---
+++ Assignments/Assignment__69/Program69_5.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function name:  Display
+//  Description:    Displays a pattern till given number by using recursive function.
+//  Input:          Void
+//  Output:         Void
+//  Author:         Mrunmai Jitendra Khadpe
+//  Date:           13/02/2026
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+    Input : 5
+    Output : a b c d e
+*/
+
+void Display(int iNo)
+{
+    static int i = 1;
+    static char ch = 'a';
+
+    if (i<=iNo)
+    {
+        printf("%c\t",ch);
+        ch = ch + 1;
+        i++;
+        Display(iNo);
+    }
+    else
+    {
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    cout << "Enter a number : ";
+    cin >> iValue;
+
+    Display(iValue);
+
+    return 0;
+}
